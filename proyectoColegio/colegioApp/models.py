9ea7192d3ca9df66cfd0c38f5fe58e4bed26320c
naroaazcona <145966781+naroaazcona@users.noreply.github.com>
2024-11-06 11:58:13 +0100

    .

--- proyectoColegio/colegioApp/models.py
+++ proyectoColegio/colegioApp/models.py
@@ -3,9 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Ciudad(models.Model):
+    nombre = models.CharField(max_length=50)
     codigoPostal = models.IntegerField()
     provincia = models.CharField(max_length=50)
     pais = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
 
 class Colegio(models.Model):
     nombre = models.CharField(max_length=100)
@@ -14,7 +18,8 @@ class Colegio(models.Model):
     cursoMax = models.CharField(max_length=50)
     modeloEstudio = models.CharField(max_length=1)
     Ciudad = models.ForeignKey(Ciudad, on_delete=models.CASCADE , related_name="colegio", null=True)   
-
+    def __str__(self):
+        return self.nombre
    
 class Profesor(models.Model):
     dni = models.CharField(max_length=9)
@@ -22,6 +27,7 @@ class Profesor(models.Model):
     fechaNac = models.DateTimeField()
     antiguedad = models.PositiveIntegerField()
     colegio = models.ForeignKey(Colegio, on_delete=models.CASCADE , related_name="profesores")   
-
+    def __str__(self):
+        return self.nombre
 
    
